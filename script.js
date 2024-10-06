7eb1b52a4fd674f534b0fd5980683337c8bc8747
johnardi <yohanesardiandk@gmail.com>
2024-10-06 23:45:58 +0700

    Create script.js for next and prev button

---
+++ script.js
@@ -0,0 +1,35 @@
+const slider = document.querySelector(".slider");
+const list = document.querySelector(".list");
+const thumbnail = document.querySelector(".thumbnail");
+const next = document.querySelector("#next");
+const prev = document.querySelector("#prev");
+
+// Event bottom click
+next.addEventListener("click", () => {
+  initSlider("next");
+});
+
+prev.addEventListener("click", () => {
+  initSlider("prev");
+});
+
+const initSlider = (type) => {
+  const sliderItems = list.querySelectorAll(".item");
+  const thumbailItems = thumbnail.querySelectorAll(".item");
+
+  if (type === "next") {
+    list.appendChild(sliderItems[0]);
+    thumbnail.appendChild(thumbailItems[0]);
+    slider.classList.add("next");
+  } else {
+    const lastItemPosition = sliderItems.length - 1;
+    list.prepend(sliderItems[lastItemPosition]);
+    thumbnail.prepend(thumbailItems[lastItemPosition]);
+    slider.classList.add("prev");
+  }
+
+  setTimeout(() => {
+    slider.classList.remove("next");
+    slider.classList.remove("prev");
+  }, 2000);
+};
